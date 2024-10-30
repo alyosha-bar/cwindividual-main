5c8a88a8ef0a850fd9a2beb64eb2774f66392452
Alyosha Baryshnikov <alohahoy@gmail.com>
2024-10-30 12:07:28 +0000

    first commit

---
+++ backend/api/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+
+    # difficulty level set choices
+    class DifficultyLevel(models.IntegerChoices):
+        EASY = 1,
+        MEDIUM = 2,
+        HARD = 3,
+
+    difficuly_rating = models.IntegerField(choices=DifficultyLevel.choices, default=DifficultyLevel.EASY)
+    weight = models.IntegerField()
+
+
+class Workout(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    date = models.DateField()
+    exercises = models.ManyToManyField(Exercise, through="Plan")
+
+
+class Plan(models.Model):
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    reps = models.IntegerField()
+    sets = models.IntegerField()
---
+++ backend/api/views.py
@@ -0,0 +1,134 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Plan, Workout, Exercise
+import json
+from django.shortcuts import get_object_or_404
+
+def test_api_view(request):
+    return JsonResponse({
+        'message': 'Good response!'
+    })
+
+
+def all_workouts(request):
+    # Query all workouts
+    # workouts = Workout.objects.all()
+
+    # # Serialize workouts into a list of dictionaries
+    # workouts_data = []
+    # for workout in workouts:
+    #     # Gather the exercises associated with each workout
+    #     exercises_data = []
+    #     plans = Plan.objects.filter(workout=workout)  # Get all plans for the current workout
+
+    #     for plan in plans:
+    #         exercises_data.append({
+    #             "exercise_id": plan.exercise.id,
+    #             "exercise_name": plan.exercise.name,
+    #             "exercise_description": plan.exercise.description,
+    #             "difficulty_rating": plan.exercise.difficuly_rating,
+    #             "weight": plan.exercise.weight,
+    #             "reps": plan.reps,
+    #             "sets": plan.sets,
+    #         })
+
+    #     workouts_data.append({
+    #         "id": workout.id,
+    #         "name": workout.name,
+    #         "description": workout.description,
+    #         "date": workout.date.strftime("%Y-%m-%d"),  # Convert date to string
+    #         "exercises": exercises_data
+    #     })
+
+    workouts = Workout.objects.all()
+
+    workout_data = [{
+            "id": workout.id,
+            "name": workout.name,
+            "description": workout.description,
+            "date": workout.date,
+        }
+        for workout in workouts
+    ]
+
+    # Return serialized data as JSON
+    return JsonResponse({
+        "workouts": workout_data
+    })
+
+def all_exercises(request):
+    exercises = Exercise.objects.all()
+    exercise_data = [
+        {
+            "id": ex.id, 
+            "name": ex.name,
+            "description": ex.description,
+            "weight": ex.weight,
+            "level": ex.difficuly_rating
+            } 
+        for ex in exercises]
+    return JsonResponse({"exercises": exercise_data})
+
+def create_workout(request):
+    # get data from req.body
+    print("Adding Data.")
+
+    if request.method == "POST":
+        data = json.loads(request.body)
+        name = data["body"].get("name")
+        description = data["body"].get("description")
+        date = data["body"].get("date")
+        exercises = data["body"].get("exercises") # contains ids of the associated exercises
+
+        # still need a way to make the reps and sets FOR EACH EXERCISE
+
+    print(name)
+    print(description)
+    print(date)
+    print(exercises)
+
+    # add into db
+
+    # create new Workout model
+    workout = Workout.objects.create(name=name, description=description, date=date)
+    # create new plan for every exercise
+    # loop through exercise,
+        # find the exercise corresponing to id
+        # use it to create a membership
+        # add default values for reps and sets
+    defaultReps=10
+    defaultSets=3
+    for id in exercises:
+        exercise = get_object_or_404(Exercise, id=id)
+        plan = Plan.objects.create(exercise=exercise, workout=workout, reps=defaultReps, sets=defaultSets)
+        plan.save()
+
+
+
+    return JsonResponse({
+        "message": "sup dude"
+    })
+
+def create_exercise(request):
+    # get data from req.body
+    print("Adding Data.")
+
+    if request.method == "POST":
+        data = json.loads(request.body)
+        name = data["body"].get("name")
+        description = data["body"].get("description")
+        weight = data["body"].get("weight")
+        diff = data["body"].get("difficulty")
+
+    print(name)
+    print(description)
+    print(weight)
+    print(diff)
+
+    # add into db - exercises model
+    exercise = Exercise(name=name, description=description, weight=weight, difficuly_rating=Exercise.DifficultyLevel.MEDIUM)
+    exercise.save()
+
+    return JsonResponse({
+        "message": "exercise added."
+    })
